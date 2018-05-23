144021f30a1ee9a56250aa0dde9cfbdaf2f26c96
Chenleiy <429719382@qq.com>
2018-05-23 22:07:17 +0800

    5.23

---
+++ 172.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        r = 0
+        while n >= 5:
+            n = n // 5
+            r+=n
+        return r 
+ 	
+
