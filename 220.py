67ccec29309a870fb40912861b5adc9090e6743b
Chenleiy <429719382@qq.com>
2018-06-06 20:20:24 +0800

    6.6

--- 220.py
+++ 220.py
@@ -6,15 +6,18 @@ class Solution:
         :type t: int
         :rtype: bool
         """
-        d = {}
-        for i in range(len(nums)):
-        	d1 = d
-        	for key in d1:
-        		if abs(nums[i]-key) <= t:
-        			if -k <= i - d1[key] <= k:
-        				return True
-        		d[key] = i
+        lenth = len(nums)
+        a = set()
+        for i in range(lenth):
+            if t==0:
+                if nums[i] in a:
+                    return True
+            else:
+                for atem in a:
+                    if abs(nums[i]-atem)<=t:
+                        return True
+            a.add(nums[i])
+            if len(a) == k+1:
+                a.remove(nums[i-k])
         return False
-s = Solution()
-print(s.containsNearbyAlmostDuplicate([1,2,3,1],3,0))
 
